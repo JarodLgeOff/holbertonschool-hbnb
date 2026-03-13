28faba3b146671ba990147d308600f99fa63750a
JarodLge <contact.jarodlge@gmail.com>
2026-03-13 14:53:14 +0100

    fix: add SQLALCHEMY_TRACK_MODIFICATIONS to DevelopmentConfig

--- part3/hbnb/config.py
+++ part3/hbnb/config.py
@@ -1,14 +1,17 @@
 import os
 
+
 class Config:
     """Base configuation class"""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
     """Developpement environnement configuration"""
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///developpement.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
