c1c5f06290529bafd5894cb8898be27a19381e75
Iglcyril <iglcyril@hotmail.fr>
2026-03-18 09:51:48 +0100

    fix: convert BaseModel to inherit from db.Model and define as abstract

--- part3/hbnb/app/models/BaseModel.py
+++ part3/hbnb/app/models/BaseModel.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from app import db
 
 
-class BaseModel:
+class BaseModel(db.Model):
+    """Abstract base model for all entities"""
+    
+    __abstract__ = True
+    
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
